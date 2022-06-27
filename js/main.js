f280c6583c4cfa6877d4728d73530f2a369b94fa
FedeTamagnone <103047702+FedeTamagnone@users.noreply.github.com>
2022-06-27 13:45:15 -0300

    Cars y lógica

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// Futuro carrito de compras
-
-
-const carritoFinal = []
+// Primera Entrega DOOM + Eventos
+/* const carritoFinal = []
 
 function producto() {
     let listado = prompt("¿Qué producto quiere llevar?: \n 1- Jabon $10 \n 2- Crema $20 \n 3- Peine $30")
@@ -50,10 +48,137 @@ function producto() {
     }
 }
 
-producto()
+//producto()
 
 const precioFinal = carritoFinal.reduce( (acc, item) =>{
     return acc += item.total;
 }, 0)
 
-console.log("El total a abonar es de $" + precioFinal + "\n Gracias por su compra");
+console.log("El total a abonar es de $" + precioFinal + "\nGracias por su compra"); */
+
+
+let carritoDeCompras = []
+//Variables globales
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const botonTerminar = document.getElementById('terminar')
+const finCompra = document.getElementById('fin-compra')
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+const buscarTipo = document.getElementById('buscarTipo')
+const buscador = document.getElementById('search')
+
+
+
+//filtro
+buscarTipo.addEventListener('change', () => {
+
+    if (buscarTipo.value == 'all') {
+        mostrarProductos(stockProductos)
+    } else {
+        let arrayNuevo = stockProductos.filter(item => item.tipo == buscarTipo.value) //array nuevo
+
+        mostrarProductos(arrayNuevo)
+    }
+})
+
+//Buscado
+
+mostrarProductos(stockProductos)
+
+//logica Ecommerce
+function mostrarProductos(array) {
+// Vacía html para mostrar solo los array filtrados
+    contenedorProductos.innerHTML = ""
+
+    for (const el of array) {
+// sino tambien puedo usar 
+//array.forEach( el => {
+/* ------------------------------ Creando card ------------------------------ */
+        let div = document.createElement('div')
+/* --------------------------- asigno class al div -------------------------- */
+        div.className = 'producto'
+
+        div.innerHTML = ` <div class="card">
+                                <div class="card-imagen">
+                                    <img src="${el.img}">
+                                    <br>
+                                    <p class="card-titulo">${el.nombre}</p>
+                                    <button id="boton${el.id}"> Agregar </button>
+                                </div>
+                                <div class="card-contenido">
+                                    <p>${el.desc}</p>
+                                    <p class="precio"> $ ${el.precio}</p>
+                                </div>
+                            </div>`
+//Agrego card en el html con append
+        contenedorProductos.appendChild(div)
+
+        let btnAgregar = document.getElementById(`boton${el.id}`)
+
+        btnAgregar.addEventListener('click', () => {
+            agregarAlCarrito(el.id);
+        })
+    }
+}
+
+
+// uso id, para seleccionar puntualmente producto
+// find busca un solo elemento
+function agregarAlCarrito(id) {
+    //repite se fija si se repite id
+    let repite = carritoDeCompras.find(el => el.id == id)
+
+    if (repite) {
+        repite.cantidad = repite.cantidad + 1 //creo una propiedad, cantidad. 
+        document.getElementById(`cantidad${repite.id}`).innerHTML = `<p id="cantidad${repite.id}">cantidad: ${repite.cantidad}</p>`
+        actualizarCarrito()
+    } else {
+        let productoAgregar = stockProductos.find(ele => ele.id === id)
+        productoAgregar.cantidad = 1
+        carritoDeCompras.push(productoAgregar) //con eso push al producto que seleccione con id al carrito
+        actualizarCarrito()
+        mostrarCarrito(productoAgregar)
+    }
+
+}
+
+
+
+function mostrarCarrito(productoAgregar) {
+
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML = `<p>${productoAgregar.nombre}</p>
+                <p>Precio: $${productoAgregar.precio}</p>
+                <p id="cantidad${productoAgregar.id}">cantidad: ${productoAgregar.cantidad}</p>
+                <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+    contenedorCarrito.appendChild(div)
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+    btnEliminar.addEventListener('click', () => {
+        if (productoAgregar.cantidad == 1) {
+            btnEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(item => item.id !== productoAgregar.id)
+            actualizarCarrito()
+        } else {
+            productoAgregar.cantidad = productoAgregar.cantidad - 1
+            document.getElementById(`cantidad${productoAgregar.id}`).innerHTML = `<p id="cantidad${productoAgregar.id}">cantidad: ${productoAgregar.cantidad}</p>`
+            actualizarCarrito()
+        }
+
+
+    })
+}
+
+
+
+
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0)
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.precio * el.cantidad), 0)
+}
