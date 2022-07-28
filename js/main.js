95859ad079f2814b4331e43947c6968149ed6c50
FedeTamagnone <103047702+FedeTamagnone@users.noreply.github.com>
2022-07-28 15:03:29 -0300

    barra

--- js/main.js
+++ js/main.js
@@ -1,62 +1,3 @@
-// Primera Entrega DOOM + Eventos
-/* const carritoFinal = []
-
-function producto() {
-    let listado = prompt("¿Qué producto quiere llevar?: \n 1- Jabon $10 \n 2- Crema $20 \n 3- Peine $30")
-    switch (listado) {
-        case "1":
-            let cantidad = prompt("¿Cuántos Jabones queres llevar?")
-            let precio = (cantidad * 10)
-            const carrito = {
-                producto: "Jabón",
-                cantidad: cantidad,
-                total: precio
-            }
-            carritoFinal.push(carrito)
-
-            break;
-
-        case "2":
-            let cantidad1 = prompt("¿Cuántas cremas queres llevar?")
-            let precio1 = (cantidad1 * 20);
-
-            const carrito1 = {
-                producto: "Crema",
-                cantidad: cantidad1,
-                total: precio1
-            }
-            carritoFinal.push(carrito1)
-            break;
-
-        case "3":
-            let cantidad2 = prompt("¿Cuántos peines queres llevar?")
-            let precio2 = (cantidad2 * 30);
-
-            const carrito2 = {
-                producto: "Peine",
-                cantidad: cantidad2,
-                total: precio2
-            }
-            carritoFinal.push(carrito2)
-            break;
-    }
-    let agregarListado=prompt("¿Querés agregar otro producto SI/NO?")
-    if (agregarListado == "si"){
-        return producto();
-    } else {
-        console.log(carritoFinal);
-    }
-}
-
-//producto()
-
-const precioFinal = carritoFinal.reduce( (acc, item) =>{
-    return acc += item.total;
-}, 0)
-
-console.log("El total a abonar es de $" + precioFinal + "\nGracias por su compra"); */
-
-
 /* ---------- variable que irá almacenando las compras en un array ---------- */
 let carritoDeCompras = JSON.parse(localStorage.getItem('datos')) || []
 
@@ -67,6 +8,10 @@ const finCompra = document.getElementById('fin-compra')
 const precioTotal = document.getElementById('precioTotal');
 const buscarTipo = document.getElementById('buscarTipo')
 const botonTerminar = document.getElementById('terminar')
+const buscador = document.getElementById('buscador')
+const btnBuscar = document.getElementById('btnBuscar')
+const barra = document.getElementById('barra')
+
 const datos = "./js/datos.json"
 //const stockProductos = JSON.stringify ()
 
@@ -79,10 +24,30 @@ buscarTipo.addEventListener('change', () => {
     } else {
         let arrayNuevo = stockProductos.filter(item => item.tipo == buscarTipo.value) //array nuevo
         mostrarProductos(arrayNuevo)
-        console.log(arrayNuevo);
     }
 })
 mostrarProductos(stockProductos)
+
+
+/* buscador.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        let arrayNuevo = stockProductos.filter(item => item.tipo == buscador.value) //array nuevo
+        mostrarProductos(arrayNuevo)
+    } 
+}) 
+mostrarProductos(stockProductos) */
+
+btnBuscar.addEventListener("click", () => {
+    if (buscador.value == "") {
+        mostrarProductos(stockProductos)
+    } else {
+        let arrayNuevo = stockProductos.filter(item => item.tipo == buscador.value) //array nuevo
+        mostrarProductos(arrayNuevo)
+        buscador.value = ""
+    }
+})
+mostrarProductos(stockProductos)
+
 
 /* ------------------------ Fn crea card de porductos ----------------------- */
 function mostrarProductos(array) {
@@ -179,7 +144,7 @@ function Carrito(productoAgregar) {
 
 
 
-//Funcion Eliminar: si cantidad es igual a 1 elimina producto, si candidad no es 1 resta productos
+    //Funcion Eliminar: si cantidad es igual a 1 elimina producto, si candidad no es 1 resta productos
     let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
     btnEliminar.addEventListener('click', () => {
         if (productoAgregar.cantidad == 1) {
